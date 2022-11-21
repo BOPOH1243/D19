7a5b627035355180364c80b7c4f7a7986c8c99bf
BOPOH1243 <tenpenni@mail.ru>
2022-11-20 22:50:09 -0500

    Starting static

--- board/models.py
+++ board/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
 class Category(models.Model):
     name = models.CharField(max_length=64, default='default')
 
@@ -11,11 +12,12 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, editable=False, )
     updated_at = models.DateTimeField(auto_now=True, editable=True, )
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
+    #FIXME добавить картинки в текст
 
 class Response(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 
 
